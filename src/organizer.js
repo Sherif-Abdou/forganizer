7fef1ffa0ed0aa021cbf2e362d21ac678dfd4f4c
Sherif <portal.meteor.master@gmail.com>
2019-09-06 20:49:49 -0700

    Initializes folders

--- src/organizer.js
+++ src/organizer.js
@@ -1,4 +1,5 @@
 const fs = require("fs")
+const path = require("path")
 
 module.exports.loadConfig = function(path) {
     let file = fs.readFileSync(path);
@@ -6,3 +7,11 @@ module.exports.loadConfig = function(path) {
     return json
 }
 
+module.exports.setUpDirectories = function(dir_path, config) {
+    config.forEach(ext => {
+        let dest_path = path.join(dir_path, ext.name);
+        if (!fs.existsSync(dest_path)) {
+            fs.mkdirSync(dest_path)
+        }
+    })
+}
--- test/test.js
+++ test/test.js
@@ -1,9 +1,18 @@
 const organizer = require("../src/organizer");
 const path = require("path");
+const fs = require("fs");
+const rimraf = require("rimraf")
 
 describe("organizer", () => {
     it("loads config file", () => {
         let config = organizer.loadConfig(path.join(__dirname,"..","config.json"))
         console.log(config)
+    })
+
+    it("creates folders based on config", () => {
+        rimraf.sync(path.join(__dirname,"files"))
+        fs.mkdirSync(path.join(__dirname,"files"))
+        let config = organizer.loadConfig(path.join(__dirname,"..","config.json"))
+        organizer.setUpDirectories(path.join(__dirname,"files"), config);
     })
 })
