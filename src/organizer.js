063781fc71a710898ebb4d8b456747740b14a078
Sherif <portal.meteor.master@gmail.com>
2019-09-07 08:52:36 -0700

    basic file sorter complete

--- src/organizer.js
+++ src/organizer.js
@@ -11,7 +11,24 @@ module.exports.setUpDirectories = function(dir_path, config) {
     config.forEach(ext => {
         let dest_path = path.join(dir_path, ext.name);
         if (!fs.existsSync(dest_path)) {
-            fs.mkdirSync(dest_path)
+            fs.mkdirSync(dest_path);
         }
-    })
+    });
+}
+
+module.exports.sortFiles = function(dir_path, config) {
+    let files = fs.readdirSync(dir_path);
+    files.forEach(file => {
+        const full_path = path.join(dir_path, file);
+        if (!fs.lstatSync(full_path).isDirectory()) {
+            const file_ext = path.parse(full_path).ext
+            config.forEach(configuration => {
+                configuration.extensions.forEach(ext => {
+                    if (ext == file_ext) {
+                        fs.renameSync(full_path, path.join(dir_path, configuration.name, file))
+                    }
+                });
+            });
+        }
+    });
 }
--- test/test.js
+++ test/test.js
@@ -6,7 +6,6 @@ const rimraf = require("rimraf")
 describe("organizer", () => {
     it("loads config file", () => {
         let config = organizer.loadConfig(path.join(__dirname,"..","config.json"))
-        console.log(config)
     })
 
     it("creates folders based on config", () => {
@@ -14,5 +13,16 @@ describe("organizer", () => {
         fs.mkdirSync(path.join(__dirname,"files"))
         let config = organizer.loadConfig(path.join(__dirname,"..","config.json"))
         organizer.setUpDirectories(path.join(__dirname,"files"), config);
+    })
+
+    it("sorts files", () => {
+        rimraf.sync(path.join(__dirname,"files"));
+        fs.mkdirSync(path.join(__dirname,"files"));
+        fs.writeFileSync(path.join(__dirname, "files", "a.png"), null);
+        fs.writeFileSync(path.join(__dirname, "files", "b.app"), null);
+        fs.writeFileSync(path.join(__dirname, "files", "c.mp3"), null);
+        let config = organizer.loadConfig(path.join(__dirname,"..","config.json"));
+        organizer.setUpDirectories(path.join(__dirname,"files"), config);
+        organizer.sortFiles(path.join(__dirname,"files"), config);
     })
 })
